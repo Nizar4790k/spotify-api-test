8d4058fcd522a0e169c4c3aba1ab5e220de2e54c
Nizar <nizar4790k@gmail.com>
2020-01-26 05:47:58 -0400

    first commit

---
+++ app/src/main/java/com/nizar4790k/spotify_api_test/MainActivity.java
@@ -0,0 +1,14 @@
+package com.nizar4790k.spotify_api_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
